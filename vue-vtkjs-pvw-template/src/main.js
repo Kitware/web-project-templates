119e71a80499df91840c0647608fd81b140ba3f3
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-02-18 16:27:19 -0500

    fix: Move vuetify to plugin

--- vue-vtkjs-pvw-template/src/main.js
+++ vue-vtkjs-pvw-template/src/main.js
@@ -4,9 +4,9 @@ import 'core-js/modules/web.immediate';
 
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Vuetify from 'vuetify';
 
 import App from 'vue-vtkjs-pvw-template/src/components/core/App';
+import Vuetify from 'vue-vtkjs-pvw-template/src/plugins/vuetify';
 import store from 'vue-vtkjs-pvw-template/src/store';
 
 /* eslint-disable-next-line import/extensions */
@@ -14,9 +14,9 @@ import 'typeface-roboto';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuex);
-Vue.use(Vuetify);
 
 new Vue({
+  Vuetify,
   store,
   render: (h) => h(App),
 }).$mount('#app');
