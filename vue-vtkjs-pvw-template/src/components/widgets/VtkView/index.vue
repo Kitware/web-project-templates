abf76b1d0cc6e2f0ef49dbe3e6afbcf03ccccd3c
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-02-19 13:56:07 -0500

    fix: Remove unused component

--- vue-vtkjs-pvw-template/src/components/widgets/VtkView/index.vue
+++
@@ -1,8 +0,0 @@
-<template
-  src="vue-vtkjs-pvw-template/src/components/widgets/VtkView/template.html"
-/>
-<style
-  module
-  src="vue-vtkjs-pvw-template/src/components/widgets/VtkView/style.css"
-/>
-<script src="vue-vtkjs-pvw-template/src/components/widgets/VtkView/script.js" />
